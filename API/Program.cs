1d6b84b4977511de2b1c8938cf65b2ae2bca31c0
hukar <k.meshoub@gmail.com>
2023-03-23 17:42:36 +0100

    cors to continue

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors();
+
 builder.AddApplicationServices();
 
 var app = builder.Build();
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
+app.UseCors(policy => policy
+                        .WithOrigins("http://localhost:5157")
+                        .AllowAnyMethod()
+                        .WithHeaders("Content-Type"));
 
 app.MapCreateDb()
     .MapRobot()
